8609948886ffc4c66e0e4e8dc4ba20cfafebfa8c
Rajesh Mondal <rajesh01749@gmail.com>
2024-01-26 16:52:01 +0600

    verify otp code and user logout (end point)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Helper\ResponseHelper;
-use App\Mail\OTPMail;
-use App\Models\User;
 use Exception;
-use Illuminate\Http\JsonResponse;
+use App\Models\User;
+use App\Mail\OTPMail;
+use App\Helper\JWTToken;
 use Illuminate\Http\Request;
+use App\Helper\ResponseHelper;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller {
@@ -22,5 +23,22 @@ class UserController extends Controller {
         } catch ( Exception $e ) {
             return ResponseHelper::Out( 'fail', $e, 200 );
         }
+    }
+
+    public function VerifyLogin( Request $request ): JsonResponse {
+        $UserEmail = $request->UserEmail;
+        $OTP = $request->OTP;
+        $verification = User::where( 'email', $UserEmail )->where( 'otp', $OTP )->first();
+        if ( $verification ) {
+            User::where( 'email', $UserEmail )->where( 'otp', $OTP )->update( ['otp' => '0'] );
+            $token = JWTToken::CreateToken( $UserEmail, $verification->id );
+            return ResponseHelper::Out( 'success', "", 200 )->cookie( 'token', $token, 60 * 24 * 30 );
+        } else {
+            return ResponseHelper::Out( 'fail', null, 401 );
+        }
+    }
+
+    function UserLogout() {
+        return redirect( '/' )->cookie( 'token', '', -1 );
     }
 }
